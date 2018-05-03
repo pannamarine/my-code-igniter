fcec7a2b4043620a5d63c353d75e8a45c11f4b53
MacBook <macbook@aca80a4d.ipt.aol.com>
2018-05-03 16:04:19 +0700

    bootsrat

---
+++ application/views/fishing-vessel/new-ship.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-4"></div>
+        <div class="card col-4">
+            <div class="card-body">
+            <h5 class="card-title">Fishing Vessel Information</h5>
+                <form action="">
+                    <div class="form-group">
+                        <label for="">Vessel Name</label>
+                        <input class="form-control" type="text">
+                    </div>
+                    <button class="btn btn-warning">Add</button>
+                    <button class="btn btn-secondary">Reset</button>
+                </form>
+        </div>
+    </div>
+    <div class="col-4"></div>
+</div>
