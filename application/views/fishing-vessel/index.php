aedf57644b170e847dbd8305dae099868574cfa0
MacBook <macbook@aca80a33.ipt.aol.com>
2018-05-04 10:27:09 +0700

    เพิ่ม card กำหนดขนาด

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -1,5 +1,53 @@
 <div class="row">
-    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: blue"></div>
+    <!-- <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: blue"></div>
     <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, Fisher!</h1>
-    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: green"></div>
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: green"></div> -->
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <div class="card">
+  <div class="card-header">
+    Vessel Name
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Country</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <div class="card">
+  <div class="card-header">
+    Vessel Name
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Country</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+ </div>
+ </div>
+      <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <div class="card">
+  <div class="card-header">
+    Vessel Name
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Country</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+ </div>
+ </div>
+      <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <div class="card">
+  <div class="card-header">
+    Vessel Name
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Country</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+ </div>
+ </div>
 </div>
